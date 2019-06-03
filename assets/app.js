c3403a25556e2c5089d51ef3cef81bb6219a0865
Disco Turkey <gelsinon.gwen@gmail.com>
2019-06-02 20:59:14 -0600

    still not working but I've got to at least start the other homework

--- assets/app.js
+++ assets/app.js
@@ -9,10 +9,8 @@ $(document).ready(function () {
     // button id to add more
     var buttonID = "";
 
-    var sport = $(this).attr("data-name");
 
-    var queryURL: "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dXsHsqNh4L9eb2FvQGxgPwJ4zGKNKxUr&limit=10";
-
+    
 
 // function to create the buttons
     function createButtons() {
@@ -23,22 +21,22 @@ $(document).ready(function () {
             btn.addClass("sport-button");
             btn.attr("id", "item" + (i + 1));
             btn.attr("data-name", sports[i]);
-            btn.text(sport[i]);
+            btn.text(sports[i]);
 //
-            if (buttonID === buttonID.attr("id")) {
+            if (buttonID === btn.attr("id")) {
                 btn.addClass("btn-info");
             }
 // display button on the page
-            $("#button-html").append(btn);
+            $("#buttons-html").append(btn);
         }
     }
 
     $("#add-sport").on("click", function (event) {
         event.preventDefault();
 
-        var sport = $("#sport-input").val().trim();
+        var sport = $("#sports-input").val().trim();
         sports.push(sport);
-        $("#food-input").val("");
+        $("#sports-input").val("");
 
         createButtons();
 
@@ -54,15 +52,53 @@ $(document).ready(function () {
         }
         $(this).addClass("btn-success");
 
+        var sport = $(this).attr("data-name");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dXsHsqNh4L9eb2FvQGxgPwJ4zGKNKxUr&limit=10";
+        
+// ajax call to connect with giphy api and pull data on to page
         $.ajax({
-            
+            url: queryURL,
+            method: "GET"
 
-        })
-    
+        }).done(function(response){
+            $("#sport-html").empty();
 
+            var data = response.data;
+            var div;
+            var img;
 
-    })
+            for(var i = 0; i < data.length; i++){
+                div = $("<div class='sport-img'>");
+                div.append("<h2>Rated: " + data[i].rating + "</h2>");
+                img = $("img class='giphy' data-state='still'"); // on initial load the giphys are supposed to be still need to add a click event for them to animate
+                img.attr({
+                    "src": data[i].images.fixed_height_still.url,
+                    "data-still": data[i].images.fixed_height.still.url,
+                    "data-animate": data[i].images.fixed_height.url
+                });
+                div.append(img);
+                $("#sport-html").append(div);
+            }
+        });
 
+    });
+// on click event to change from still to animate and back
+    $(document).on("click", "giphy",function(){
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+
+		}
+		else {
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+        }
+    });
+
+    createButtons();
 });
 
 
